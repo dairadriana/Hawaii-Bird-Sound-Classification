36289bd2bcd61e228d8b62a16e26ee71f4aff0bf
Daira Rodriguez <35277731+dairadriana@users.noreply.github.com>
2026-04-29 00:32:25 -0600

    Update unrelated_data.py

--- experiments/unrelated_data.py
+++ experiments/unrelated_data.py
@@ -7,7 +7,6 @@ import tensorflow as tf
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from src.config import Config
-from src.audio_processing import add_noise_to_audio, audio_to_logmel
 
 
 config = Config()
@@ -16,9 +15,7 @@ MODEL_PATH = config.get("paths", "best_model_path")
 PROCESSED_DIR = config.get("paths", "processed_dir")
 
 
-# =========================
-# DATOS DE VAL
-# =========================
+# datos de val
 X = np.load(os.path.join(PROCESSED_DIR, "X.npy"))
 val_idx = np.load(os.path.join(PROCESSED_DIR, "main/main_val_idx.npy"))
 
@@ -28,9 +25,7 @@ class_names = np.load(os.path.join(PROCESSED_DIR, "main_classes.npy"))
 print("Clases de datos de validacion")
 print(class_names)
 
-# =========================
-# DATOS SIN RELACION
-# =========================
+# unrelated 
 exp_val_idx = np.load(os.path.join(PROCESSED_DIR, "exp/exp_val_idx.npy"))
 exp_val = X[exp_val_idx]
 
@@ -39,16 +34,11 @@ exp_classes = np.load(os.path.join(PROCESSED_DIR, "exp_classes.npy"))
 print("Clases de datos sin relacion")
 print(exp_classes)
 
-# =========================
-# CARGAR MODELO
-# =========================
 
+#Modelo:
 model = tf.keras.models.load_model(MODEL_PATH)
 
-# =========================
-# EVALUAR
-# =========================
-
+# EVAL
 # Datos de validacion
 probs = model.predict(X_val)
 y_pred = np.argmax(probs, axis=1)
@@ -56,10 +46,6 @@ y_pred = np.argmax(probs, axis=1)
 # Datos sin relacion
 exp_probs = model.predict(exp_val)
 exp_y_pred = np.argmax(exp_probs, axis=1)
-
-# =========================
-# ANALISIS
-# =========================
 
 exp_conf = np.max(exp_probs, axis=1)
 val_conf = np.max(probs, axis=1)
@@ -70,7 +56,7 @@ unique, counts = np.unique(y_pred, return_counts=True)
 for cls, count in zip(unique, counts):
     print(f"Clase {class_names[cls]}: {count} muestras")
 
-# Confianza del modelo en datos sin relación
+# Confianza del modelo en datos sin relacion
 print("\nConfianza del modelo en datos sin relacion")
 print(f"Promedio: {np.mean(exp_conf):.4f}")
 print(f"Desviacion estandar: {np.std(exp_conf):.4f}")
@@ -78,7 +64,7 @@ print(f"Mediana: {np.median(exp_conf):.4f}")
 print(f"Minimo: {np.min(exp_conf):.4f}")
 print(f"Maximo: {np.max(exp_conf):.4f}")
 
-# Comparación con datos de validación
+# Comparacion con datos de validación
 print("\nConfianza del modelo en datos conocidos")
 print(f"Promedio: {np.mean(val_conf):.4f}")
 print(f"Desviacion estandar: {np.std(val_conf):.4f}")
